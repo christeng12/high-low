7f90d6f9c1b681874c93b5db08e0342d88dc7762
Christopher Teng <c.s.teng@wustl.edu>
2022-06-09 16:39:50 -0500

    added buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+import React, {Component} from 'react';
+
+class Keep extends Component {
+  render() {
+    return(
+      <button className="keep">Keep</button>
+    );
+  }
 }
 
-export default App;
+class High extends Component {
+  render() {
+    return(
+      <button className="high">High</button>
+    );
+  }
+}
+
+class Low extends Component {
+  render() {
+    return(
+      <button className="low">Low</button>
+    );
+  }
+}
+
+class Game extends Component {
+  render() {
+    return (
+      <div className="Game">
+        <High />
+        <Low />
+        <Keep />
+      </div>
+    );
+  }
+}
+export default Game;
